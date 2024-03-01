c6544f6584a3d156043ffcd2ce885b82b15eab29
Daniel Rashevsky <drashe@uw.edu>
2024-03-01 02:05:38 -0800

    Implemented mini-batch-k-means tauri command using linfa-clustering

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,14 +1,46 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use linfa::DatasetBase;
+use linfa::traits::FitWith;
+use linfa_clustering::{/*KMeansParams,*/ KMeans, IncrKMeansError};
+use ndarray::Array2;
+use ndarray_rand::rand::SeedableRng;
+use rand_xoshiro::Xoshiro256Plus;
+
 fn main() {
   tauri::Builder::default()
-    .invoke_handler(tauri::generate_handler![greet])
+    .invoke_handler(tauri::generate_handler![cluster])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
 
 #[tauri::command]
-fn greet(name: &str) -> String {
-   format!("Hello, {}!", name)
+fn cluster(embeddings: Vec<f32>,
+           embeddings_cnt: usize,
+           embeddings_dims: usize,
+           batch_size: usize,
+           n_clusters: usize, ) -> String {
+   
+    // Code taken from https://docs.rs/linfa-clustering/latest/linfa_clustering/struct.KMeans.html#tutorial
+    let mut rng = Xoshiro256Plus::seed_from_u64(42);
+    let data = Array2::from_shape_vec((embeddings_cnt, embeddings_dims), embeddings).unwrap();
+    let observations = DatasetBase::from(data.clone()).shuffle(&mut rng);                       // Imports the 2d matrix
+    let clf = KMeans::params_with_rng(n_clusters, rng.clone()).tolerance(1e-3);                 // Sets up kmeans
+
+    // Repeatedly run fit_with on every batch in the dataset until we have converged.
+    // fit_with implements the mini-batch-k-means algorithm
+    let model = observations
+        .sample_chunks(batch_size)
+        .cycle()
+        .try_fold(None, |current, batch| {
+            match clf.fit_with(current, &batch) {
+                Ok(model) => Err(model),
+                Err(IncrKMeansError::NotConverged(model)) => Ok(Some(model)),
+                Err(err) => panic!("unexpected kmeans error: {}", err),
+            }
+        })
+        .unwrap_err();
+
+    "centroids, assignments".to_string()
 }
