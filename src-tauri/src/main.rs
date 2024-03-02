f6b86636a6c8b64c096a2bf80b74d636be555709
Daniel Rashevsky <drashe@uw.edu>
2024-03-02 01:51:49 -0800

    Made cluster method return data back to javascript

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,9 +1,15 @@
+// Portions of this code taken from 
+// - https://docs.rs/linfa-clustering/latest/linfa_clustering/struct.KMeans.html#tutorial
+// - https://github.com/rust-ml/linfa/blob/master/algorithms/linfa-clustering/examples/kmeans.rs
+// under license: https://github.com/rust-ml/linfa/blob/master/LICENSE-MIT
+
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 use linfa::DatasetBase;
 use linfa::traits::FitWith;
-use linfa_clustering::{/*KMeansParams,*/ KMeans, IncrKMeansError};
+use linfa::traits::Predict;
+use linfa_clustering::{KMeans, IncrKMeansError};
 use ndarray::Array2;
 use ndarray_rand::rand::SeedableRng;
 use rand_xoshiro::Xoshiro256Plus;
@@ -23,17 +29,16 @@ fn cluster(embeddings: Vec<f32>,
            embeddings_cnt: usize,
            embeddings_dims: usize,
            batch_size: usize,
-           n_clusters: usize, ) -> String {
+           n_clusters: usize, ) -> (Vec<f32>, Vec<f32>, Vec<u64>) {
    
-    // Code taken from https://docs.rs/linfa-clustering/latest/linfa_clustering/struct.KMeans.html#tutorial
     let mut rng = Xoshiro256Plus::seed_from_u64(42);
     let data = Array2::from_shape_vec((embeddings_cnt, embeddings_dims), embeddings).unwrap();
-    let observations = DatasetBase::from(data.clone()).shuffle(&mut rng);               // Imports the 2d matrix
+    let dataset = DatasetBase::from(data.clone()).shuffle(&mut rng);                    // Imports the 2d embeddings matrix
     let clf = KMeans::params_with(n_clusters, rng.clone(), CosDist).tolerance(1e-3);    // Sets up kmeans
 
     // Repeatedly run fit_with on every batch in the dataset until we have converged.
     // fit_with implements the mini-batch-k-means algorithm
-    let model = observations
+    let model = dataset
         .sample_chunks(batch_size)
         .cycle()
         .try_fold(None, |current, batch| {
@@ -45,5 +50,14 @@ fn cluster(embeddings: Vec<f32>,
         })
         .unwrap_err();
 
-    "centroids, assignments".to_string()
+    let dataset = model.predict(dataset);
+    let centroids = model.centroids().clone();
+    let DatasetBase {
+        records, targets, ..
+    } = dataset;
+
+    // Return 2d matrices of centroids, original embeddings, cluster assignments
+    (centroids.into_raw_vec(), 
+     records.into_raw_vec(), 
+     targets.map(|&x| x as u64).into_raw_vec())
 }
