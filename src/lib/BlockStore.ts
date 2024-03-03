28e07ba15b03ffdc73dcb8d37746f3e038a6037c
Daniel Rashevsky <drashe@uw.edu>
2024-03-03 15:20:53 -0800

    Added function to search blockstore by embedding

--- src/lib/BlockStore.ts
+++ src/lib/BlockStore.ts
@@ -1,11 +1,30 @@
-interface Block {
+export interface Block {
     text: string, 
     vec: number[], 
     timestamp: number
 }
 
 export class BlockStore extends Map<number, Block> {
+    private _embeddingKeys: Map<string, number>;
 
+    constructor() {
+        super();
+        this._embeddingKeys = new Map<string, number>();
+    }
+
+    set(n: number, b: Block): this {
+        super.set(n, b);
+        this._embeddingKeys.set(JSON.stringify(b.vec.slice(0, 10)), n);
+        return this;
+    }
+
+    // Not sure how robust this will be, relies on embedding model not changing.
+    // Seems to work for now.
+    getByEmbedding(embedding: number[]): Block | undefined {
+        let hash = this._embeddingKeys.get(JSON.stringify(embedding.slice(0, 10)));
+        if (!hash) return undefined;
+        return super.get(hash);
+    }
 }
 
 // https://stackoverflow.com/questions/7616461/generate-a-hash-from-string-in-javascript
