45fda301c34c58b43c042f79d985fb189475d4b1
Daniel Rashevsky <drashe@uw.edu>
2024-03-07 01:25:21 -0800

    Fixed timestamp ordering of nested list to not mutate arrays, put root blocks at end

--- src/routes/NestedListNode.svelte
+++ src/routes/NestedListNode.svelte
@@ -7,19 +7,21 @@
 
     function sortByTimestamp(a: NavTreeNode, b: NavTreeNode) {
         let e1 = store.getByEmbedding(a.embedding) , e2 = store.getByEmbedding(b.embedding);
-        return (e1 && e2) ? e1.timestamp - e2.timestamp : 0;
+        if (e1 && e2) return e1.timestamp - e2.timestamp;
+        if (e1 || e2) return (e1) ? 1 : -1;
+        return 0;
     }
 </script>
 
 <div class="w-full">
     {#if currNode.children.length > 0 && currNode.embedding.length == 0}
-        {#each currNode.children.sort(sortByTimestamp) as child}
+        {#each [...currNode.children].sort(sortByTimestamp) as child}
             <svelte:self {store} currNode={child}/>
         {/each}
     {:else if currNode.children.length > 0}
         <div class="w-full h-100">Cluster</div>
         <div class="w-full pt-1 pl-4">
-            {#each currNode.children.sort(sortByTimestamp) as child}
+            {#each [...currNode.children].sort(sortByTimestamp) as child}
                 <svelte:self {store} currNode={child}/>
             {/each}
         </div>
