5660c78d82b4f573df6cd1556278f44385380969
Daniel Rashevsky <drashe@uw.edu>
2024-03-18 16:24:29 -0700

    Added navbar, changed background styling

--- src/routes/InputBar.svelte
+++ src/routes/InputBar.svelte
@@ -36,11 +36,12 @@
     }
 
     $: text && inputBarUpdated();   // this line doesn't fire when the whole textbox is emptied
-    $: word_count = text.split(/\s+/).length;
+    $: word_count = text.split(/\s+/).filter(token => token.length > 0).length;
 </script>
 
 <div class="fixed bottom-0 w-full h-[15%] p-3">
-    <textarea id="input-bar" class="w-full h-full p-1 border border-gray-500 rounded-md resize-none" 
+    <textarea id="input-bar" 
+        class="w-full h-full bg-zinc-50 p-1 border border-gray-500 rounded-md resize-none" 
         placeholder="Space to type or search. Enter to submit. Shift + enter for newline." 
         bind:value={text}
         on:keydown={handleKey}/>
--- src/routes/NavBar.svelte
+++ src/routes/NavBar.svelte
@@ -1,3 +1,24 @@
-<div class="fixed top-0 w-full h-[5%] flex flex-wrap content-center">
-    <h3 class="m-0">TensorNote</h3>
+<script>
+    import img from "$lib/logo.png";
+
+    let closed = true;
+</script>
+
+<div class="fixed top-2 right-6 flex flex-col items-end">
+    <button class="w-6 h-6 " on:click={() => closed = !closed}>
+        <svg viewBox="0 0 24 24" class="w-full h-full fill-slate-700">
+            <path d="M4 18L20 18" stroke="#000000" stroke-width="1.5" stroke-linecap="round"></path> 
+            <path d="M4 12L20 12" stroke="#000000" stroke-width="1.5" stroke-linecap="round"></path> 
+            <path d="M4 6L20 6" stroke="#000000" stroke-width="1.5" stroke-linecap="round"></path> 
+        </svg>        
+    </button>
+    {#if !closed}
+        <div class="bg-stone-50 min-w-40 shadow-lg">
+            <div class="px-4 py-3 flex items-center">
+                <img src="{img}" class="w-5 h-5 mr-2" alt="TensorNote"/>
+                <p>TensorNote</p>
+            </div>
+            <button class="px-4 py-3 block hover:bg-zinc-100">Copy all to clipboard</button>
+        </div>
+    {/if}
 </div>
