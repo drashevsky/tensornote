a409cf294041095d3bbdcd78db44d344a7558823
Daniel Rashevsky <drashe@uw.edu>
2024-03-18 17:37:17 -0700

    Refactored store/NavTree helper functions into separate file, added printtree

--- src/lib/PrintTree.ts
+++ src/lib/PrintTree.ts
@@ -1,5 +1,9 @@
 import { BlockStore } from "./BlockStore";
-import { NavTree, type NavTreeNode } from "./NavTree";
+import { getTfIdf, getTopNKeywords } from "./Keywords";
+import { NavTree, csim, type NavTreeNode } from "./NavTree";
+
+const NUM_KEYWORDS = 5;
+const UNTITLED_SUBLIST_NAME = "Untitled Sublist";
 
 export function printTree(store: BlockStore, 
                           tree: NavTree, 
@@ -12,10 +16,44 @@ export function printTree(store: BlockStore,
 
     // recursive case: it's a cluster
     } else {
-        let clusterStr = depth + "- cluster\n";
+        let clusterStr = depth + "- " + getTitle(store, node => tree.getDescendants(node), currNode) + "\n";
         currNode.children.forEach((child) => {
             clusterStr += printTree(store, tree, child, depth + "\t");
         });
         return clusterStr;
     }
+}
+
+// given a blockstore, function to find descendant embeddings, and a cluster node,
+// generate a title for the cluster node.
+export function getTitle(store: BlockStore, 
+                         descFunc: (node: NavTreeNode) => number[][], 
+                         node: NavTreeNode): string {
+    if (node.children.length == 0 || node.embedding.length == 0)
+        return UNTITLED_SUBLIST_NAME;
+    
+    let descendants = descFunc(node);
+    let freq_matrix = [];
+
+    for (let i = 0; i < descendants.length; i++) {
+        let block = store.getByEmbedding(descendants[i]);
+        if (block) freq_matrix.push(block.keywords);
+    }
+
+    let topKeywords = getTopNKeywords(getTfIdf(freq_matrix, false), NUM_KEYWORDS).map(([a, b]) => a);
+    return topKeywords.length > 0 ? topKeywords.join(", ") : UNTITLED_SUBLIST_NAME;
+}
+
+// if both nodes are blocks, sort by timestamp. if both nodes are clusters, sort by distance
+// to parent. otherwise, put clusters first.
+export function sortNodes(store: BlockStore, parent: NavTreeNode, a: NavTreeNode, b: NavTreeNode) {
+    let e1 = store.getByEmbedding(a.embedding);
+    let e2 = store.getByEmbedding(b.embedding);
+    let p_embedding = parent.embedding;
+
+    if (e1 && e2) return e1.timestamp - e2.timestamp;                   // 2 blocks
+    if (e1 || e2) return e1 ? 1 : -1;                                   // block/cluster
+    if (p_embedding.length == 0) return 0;                              // root node item
+    return (1 - csim(p_embedding, a.embedding)) -                       // 2 clusters
+           (1 - csim(p_embedding, b.embedding));   
 }
--- src/routes/NestedListNode.svelte
+++ src/routes/NestedListNode.svelte
@@ -1,55 +1,24 @@
 <script lang="ts">
     import type { BlockStore } from "$lib/BlockStore";
-    import { getTfIdf, getTopNKeywords } from "$lib/Keywords";
-    import { csim, type NavTreeNode } from "$lib/NavTree";
+    import type { NavTreeNode } from "$lib/NavTree";
+    import { getTitle, sortNodes } from "$lib/PrintTree";
     import { createEventDispatcher } from 'svelte';
     import { slide } from "svelte/transition";
 
-    const NUM_KEYWORDS = 5;
     const CURSOR_ACTIVE = "border-red-400 border-b-2";
     const SVG_ARROW = `M13.75 9.56879C14.0833 9.76124 14.0833 10.2424 13.75
                        10.4348L8.5 13.4659C8.16667 13.6584 7.75 13.4178 7.75
                        13.0329L7.75 6.97072C7.75 6.58582 8.16667 6.34525 8.5 6.5377L13.75 9.56879Z`;
-    const UNTITLED_SUBLIST_NAME = "Untitled Sublist";
 
     export let store: BlockStore;
     export let currNode: NavTreeNode;
     export let cursorNode: NavTreeNode;
     export let descFunc: (node: NavTreeNode) => number[][];
     const dispatch = createEventDispatcher();
+    const sortFunc = (a: NavTreeNode, b: NavTreeNode) => sortNodes(store, currNode, a, b);
 
     let closed = false;
-    let title = UNTITLED_SUBLIST_NAME;
-
-    // if both nodes are blocks, sort by timestamp. if both nodes are clusters, sort by distance
-    // to parent. otherwise, put clusters first.
-    function sortNodes(a: NavTreeNode, b: NavTreeNode) {
-        let e1 = store.getByEmbedding(a.embedding);
-        let e2 = store.getByEmbedding(b.embedding);
-        let parent = currNode.embedding;
-
-        if (e1 && e2) return e1.timestamp - e2.timestamp;                   // 2 blocks
-        if (e1 || e2) return e1 ? 1 : -1;                                   // block/cluster
-        if (parent.length == 0) return 0;                                   // root node item
-        return (1 - csim(parent, a.embedding)) -                            // 2 clusters
-               (1 - csim(parent, b.embedding));   
-    }
-
-    function getTitle(node: NavTreeNode): string {
-        if (node.children.length == 0 || node.embedding.length == 0)
-            return UNTITLED_SUBLIST_NAME;
-        
-        let descendants = descFunc(node);
-        let freq_matrix = [];
-
-        for (let i = 0; i < descendants.length; i++) {
-            let block = store.getByEmbedding(descendants[i]);
-            if (block) freq_matrix.push(block.keywords);
-        }
-
-        let topKeywords = getTopNKeywords(getTfIdf(freq_matrix, false), NUM_KEYWORDS).map(([a, b]) => a);
-        return topKeywords.length > 0 ? topKeywords.join(", ") : UNTITLED_SUBLIST_NAME;
-    }
+    let title = "";
 
     function handleKey(e: KeyboardEvent) {
         if (e.key == "Escape") {
@@ -63,12 +32,12 @@
         dispatch('removenode', {node: currNode});
     }
 
-    $: title = getTitle(currNode);
+    $: title = getTitle(store, descFunc, currNode);
 </script>
 
 <div class="w-full pt-1 {(cursorNode == currNode) ? CURSOR_ACTIVE : ""}">
     {#if currNode.children.length > 0 && currNode.embedding.length == 0}
-        {#each [...currNode.children].sort(sortNodes) as child}
+        {#each [...currNode.children].sort(sortFunc) as child}
             <svelte:self {store} currNode={child} {cursorNode} {descFunc} on:removenode/>
         {/each}
     {:else if currNode.children.length > 0}
@@ -87,7 +56,7 @@
         </div>
         {#if !closed}
             <div class="w-full pl-10" transition:slide>
-                {#each [...currNode.children].sort(sortNodes) as child}
+                {#each [...currNode.children].sort(sortFunc) as child}
                     <svelte:self {store} currNode={child} {cursorNode} {descFunc} on:removenode/>
                 {/each}
             </div>
