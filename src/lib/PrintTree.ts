4110a4e8e5eac7e6697c871dfb8a2f64d64fa4a6
Daniel Rashevsky <drashe@uw.edu>
2024-03-18 17:14:51 -0700

    Revamped printtree

--- src/lib/PrintTree.ts
+++ src/lib/PrintTree.ts
@@ -1,32 +1,21 @@
 import { BlockStore } from "./BlockStore";
 import { NavTree, type NavTreeNode } from "./NavTree";
 
-async function printTreeHelper(texts: Map<string, string>, currNode: NavTreeNode, depth: string) {
+export function printTree(store: BlockStore, 
+                          tree: NavTree, 
+                          currNode: NavTreeNode, 
+                          depth: string): string {
         
     // base case: it's a block
     if (currNode.children.length == 0) {
-        let query = JSON.stringify(currNode.embedding.slice(0, 10));
-        console.log(depth, texts.get(query));
+        return depth + "- " + store.getByEmbedding(currNode.embedding)?.text.trim() + "\n";
 
     // recursive case: it's a cluster
     } else {
-        console.log(depth + "cluster");
+        let clusterStr = depth + "- cluster\n";
         currNode.children.forEach((child) => {
-            printTreeHelper(texts, child, depth + "----");
+            clusterStr += printTree(store, tree, child, depth + "\t");
         });
+        return clusterStr;
     }
-}
-
-export async function printTree(store: BlockStore) {
-    let tree = new NavTree();
-    let embeddings: number[][] = [];
-    let texts = new Map<string, string>();
-    Array.from(store.values()).forEach(block => {
-        embeddings.push(block.vec);
-        texts.set(JSON.stringify(block.vec.slice(0, 10)), block.text);
-    });
-    let timestamp = Date.now();
-    await tree.buildTree(embeddings);
-    printTreeHelper(texts, tree.root, "");
-    console.log(Date.now() - timestamp, "ms");
 }
